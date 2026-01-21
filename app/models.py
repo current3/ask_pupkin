665f1f5cfd5a0945ecedf83013fc5c8f6fd122b5
currentU <urazgaliev.dmitrii02102005@gmail.com>
2026-01-21 14:44:09 +0300

    Added DB models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    nickname = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.nickname or self.user.username
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-created_at')
+
+    def hot(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='questions')
+    created_at = models.DateTimeField(default=timezone.now)
+    rating = models.IntegerField(default=0)
+
+    tags = models.ManyToManyField(Tag, related_name='questions', blank=True)
+
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        from django.urls import reverse
+        return reverse('question_detail', kwargs={'pk': self.pk})
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='answers')
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    is_correct = models.BooleanField(default=False)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Answer to {self.question_id} by {self.author}'
+
+
+class QuestionLike(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='question_likes')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='likes')
+    value = models.SmallIntegerField(default=1)  # +1 или -1
+
+    class Meta:
+        unique_together = ('user', 'question')
+
+
+class AnswerLike(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='answer_likes')
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='likes')
+    value = models.SmallIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('user', 'answer')
